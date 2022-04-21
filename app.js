a1c0886fe702dde6af24127acd7e88fd5a2d3ca5
juanihagel <94989828+juanihagel@users.noreply.github.com>
2022-04-20 22:44:11 -0300

    avances

--- app.js
+++ app.js
@@ -16,14 +16,16 @@ selecTipo.addEventListener("change", () => {
         array.forEach(StockProductos => {
             let div = document.createElement("div")
             div.className = "producto"
-            
-            div.innerHTML =`               
+
+            div.innerHTML = `               
                 <div class="card" style="width: 18rem;">
+                <img>${StockProductos.Image}<img>
                     <span>${StockProductos.nombre} </span>
                     <a id="boton${StockProductos.id}"></a>
                     <div class="card-body">
                         <p>${StockProductos.desc} </p>
                         <p>${StockProductos.precio} </p>
+                        <i class="fa-solid fa-cart-plus"></i>
                     </div>
                 </div>`
             contenedorProductos.appendChild(div);
